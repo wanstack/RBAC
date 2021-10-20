13a77731c03d733021dbc2d331dd6617c0174c1c
yujing@cyberpeace.cn <yujing@cyberpeace.cn>
2021-10-20 14:16:16 +0800

    初始化

---
+++ apps/system/views_user.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from .forms import LoginForm
+
+
+class IndexView(View):
+
+    def get(self, request):
+        return render(request, 'index.html')
+
+
+class LoginView(View):
+
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return render(request, 'system/users/login.html')
+        else:
+            return HttpResponseRedirect('/')
+
+    def post(self, request):
+        redirect_to = request.GET.get('next', '/')
+        login_form = LoginForm(request.POST)
+        ret = dict(login_form=login_form)
+        if login_form.is_valid():
+            user_name = request.POST['username']
+            pass_word = request.POST['password']
+            user = authenticate(username=user_name, password=pass_word)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect(redirect_to)
+                else:
+                    ret['msg'] = '用户未激活！'
+            else:
+                ret['msg'] = '用户名或密码错误！'
+        else:
+            ret['msg'] = '用户和密码不能为空！'
+        return render(request, 'system/users/login.html', ret)
+
+
+class LogoutView(View):
+
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse('login'))
